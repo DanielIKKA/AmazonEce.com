897e38951461124fee0bbdfbd95ba6b825d3b56d
Daniel IKKA <daniel.ikka@edu.ece.fr>
2019-04-30 23:29:29 +0200

    - logo abous us and profil revu - top header presque au point

--- php/headerTemplate.php
+++ php/headerTemplate.php
@@ -2,25 +2,37 @@
 <link rel="stylesheet" type="text/css" href="../Css/HeaderStyleSheet.css">
 <header id="wrapper" class="border">
     <div id="top_header">
-        <div id="menu_tool">
-            <p> menu </p>
+
+        <div id="item1_top" class="item_top">
+            <div id="menu_tool">
+                <p> menu </p>
+            </div>
         </div>
 
-        <div id="logo">
+        <div id="item2_top" class="item_top">
             <img class="contain" src="../Assets/WebSiteResources/Logo.png" alt="logo du site">
         </div>
-        <div id="icons">
-            <div id="icon_profil">
-                <img class="contain" src="../Assets/WebSiteResources/My_account_icon.png">
+
+        <div id="item3_top" class="item_top">
+            <div id="icons">
+                <div class="pointer" id="icon_profil">
+                    <img class="contain" src="../Assets/WebSiteResources/My_account_icon.png">
+                    <p>Mon compte</p>
+                </div>
+                <div class="pointer" id="icon_about">
+                    <img class="contain" src="../Assets/WebSiteResources/About_us_icon.png">
+                    <p>A propos de nous</p>
+                </div>
             </div>
-            <div id="icon_about">
-                <img class="contain" src="../Assets/WebSiteResources/About_us_icon.png">
-            </div>
+            <hr id="separator_vertical">
+                <div class="pointer" id="cart">
+                    <div id="icon_cart">
+                        <img class="contain" src="../Assets/WebSiteResources/Shopping_cart_icon.png">
+                    </div>
+                    <h1> panier </h1>
+                </div>
         </div>
-        <hr id="separator_vertical">
-        <div id="cart">
-            <p> panier </p>
-        </div>
+
     </div>
 
     <hr id="separator_top_bottom">
