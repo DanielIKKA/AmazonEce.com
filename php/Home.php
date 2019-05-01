9f3db3cd44f97724374c9940b6c726a3901ce14f
Daniel IKKA <daniel.ikka@edu.ece.fr>
2019-05-01 17:01:11 +0200

    - home page done

--- php/Home.php
+++ php/Home.php
@@ -10,59 +10,62 @@
     <?php include ("headerTemplate.php") ; ?>
 
     <!-- main body home page -->
-    <main id="main_wrapper">
-        <div class="cat_item" id="cat_item_1">
-            <a href="#" class="pic">
-                <div class="rect_hover fit">
-                    <h1>Livres</h1>
-                </div>
-                <img class="fit" src="../Assets/WebSiteResources/images/books_image.jpg">
-            </a>
-            <section class="section_side">
-                <a href="#">Vente Flash</a>
-                <a href="#">Top des Ventes</a>
-                <a href="#">Notre Selection</a>
-            </section>
+    <main>
+        <div id="main_wrapper">
+            <div class="cat_item" id="cat_item_1">
+                <a href="#" class="pic">
+                    <div class="rect_hover fit">
+                        <h1>Livres</h1>
+                    </div>
+                    <img class="fit" src="../Assets/WebSiteResources/images/books_image.jpg">
+                </a>
+                <section class="section_side">
+                    <a href="#">Vente Flash</a>
+                    <a href="#">Top des Ventes</a>
+                    <a href="#">Notre Selection</a>
+                </section>
+            </div>
+            <div class="cat_item" id="cat_item_2">
+                <a href="#" class="pic">
+                    <div class="rect_hover fit">
+                        <h1>Musiques</h1>
+                    </div>
+                    <img class="fit" src="../Assets/WebSiteResources/images/music_image.jpg">
+                </a>
+                <section class="section_side">
+                    <a href="#">Vente Flash</a>
+                    <a href="#">Top des Ventes</a>
+                    <a href="#">Notre Selection</a>
+                </section>
+            </div>
+            <div class="cat_item" id="cat_item_3">
+                <a href="#" class="pic">
+                    <div class="rect_hover fit">
+                        <h1>Vêtements</h1>
+                    </div>
+                    <img class="fit" src="../Assets/WebSiteResources/images/clothes_image.jpg">
+                </a>
+                <section class="section_side">
+                    <a href="#">Vente Flash</a>
+                    <a href="#">Top des Ventes</a>
+                    <a href="#">Notre Selection</a>
+                </section>
+            </div>
+            <div class="cat_item" id="cat_item_4">
+                <a href="#" class="pic">
+                    <div class="rect_hover fit">
+                        <h1>Sport et Loisirs</h1>
+                    </div>
+                    <img class="fit" src="../Assets/WebSiteResources/images/sports_image.jpeg">
+                </a>
+                <section class="section_side">
+                    <a href="#">Vente Flash</a>
+                    <a href="#">Top des Ventes</a>
+                    <a href="#">Notre Selection</a>
+                </section>
+            </div>
         </div>
-        <div class="cat_item" id="cat_item_2">
-            <a href="#" class="pic">
-                <div class="rect_hover fit">
-                    <h1>Musiques</h1>
-                </div>
-                <img class="fit" src="../Assets/WebSiteResources/images/music_image.jpg">
-            </a>
-            <section class="section_side">
-                <a href="#">Vente Flash</a>
-                <a href="#">Top des Ventes</a>
-                <a href="#">Notre Selection</a>
-            </section>
-        </div>
-        <div class="cat_item" id="cat_item_3">
-            <a href="#" class="pic">
-                <div class="rect_hover fit">
-                    <h1>Vêtements</h1>
-                </div>
-                <img class="fit" src="../Assets/WebSiteResources/images/clothes_image.jpg">
-            </a>
-            <section class="section_side">
-                <a href="#">Vente Flash</a>
-                <a href="#">Top des Ventes</a>
-                <a href="#">Notre Selection</a>
-            </section>
-        </div>
-        <div class="cat_item" id="cat_item_4">
-            <a href="#" class="pic">
-                <div class="rect_hover fit">
-                    <h1>Sport et Loisirs</h1>
-                </div>
-                <img class="fit" src="../Assets/WebSiteResources/images/sports_image.jpeg">
-            </a>
-            <section class="section_side">
-                <a href="#">Vente Flash</a>
-                <a href="#">Top des Ventes</a>
-                <a href="#">Notre Selection</a>
-            </section>
-        </div>
+
     </main>
 
     <!-- import the footer template -->
