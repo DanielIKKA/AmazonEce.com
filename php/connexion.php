6946e03661c92f8b9e57aa1e485b9f3bf72c3279
Daniel IKKA <daniel.ikka@edu.ece.fr>
2019-05-01 18:05:07 +0200

    - connexion page presque fini

--- php/connexion.php
+++ php/connexion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title> ECEamazon.com </title>
     <link rel="stylesheet" type="text/css" href="../Css/GeneralStylesheet.css">
-    <link rel="stylesheet" type="text/css" href="../Css/HomeStylesheet.css">
+    <link rel="stylesheet" type="text/css" href="../Css/ConnexionSylesheet.css">
 </head>
 <body>
 <!-- import the header template -->
@@ -14,14 +14,15 @@
 <main>
     <div id="main_wrapper">
         <div class="connexion">
-            <form method="post" action="traitement_connexion.php">
-                <label for="email">Votre email </label><input type="text" name="email" id="email"><br/>
-                <label for="password">Votre mot de passe </label><input type="password" name="password" id="password"><br/>
-                <p><br/><input type="submit" value="Se connecter"/></p>
+            <h1>Se Connecter</h1>
+            <div id="form_wrapper" method="post" action="traitement_connexion.php">
+                <input class="input_text" id="email" type="text" name="email">
+                <input class="input_text" id="password" type="password" name="password">
+                <input type="submit" value="Se connecter"/>
+                <a class="input_btn" href="inscription.php">S'enregistrer</a>
             </form>
         </div>
     </div>
-
 </main>
 
 <!-- import the footer template -->
