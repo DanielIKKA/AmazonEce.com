cf15813e93e7e9f19ca6ddbad020fbe90268246a
LAPTOP-IR15VP0N\arnau <arnaud.emprin@gmail.com>
2019-05-01 11:38:29 +0200

    Paiement et enregistrement des commandes

---
+++ php/Pay_Order_Process.php
@@ -0,0 +1,65 @@
+<?php
+
+    $info_bank = false;
+    $info_perso = false;
+
+    if(isset($_POST['surname']) AND isset($_POST['firstname']) AND isset($_POST['adress']) AND isset($_POST['city'])and isset($_POST['country']) and isset($_POST['postal_code'])AND isset($_POST['tel']))
+    {
+        $info_perso = true;
+    }else echo "Il manque des infos personelles";
+
+    if(isset($_POST['type']) AND isset($_POST['number']) AND isset($_POST['name']) AND isset($_POST['expiracy_date'])and isset($_POST['security_code']))
+    {
+        $info_bank = true;
+    }else echo "Il manque des infos bancaires";
+
+    if($info_bank and $info_perso)
+    {
+        $surname = $_POST['surname'];
+        $firstname = $_POST['firstname'];
+        $adress = $_POST['adress'];
+        $city = $_POST['city'];
+        $country = $_POST['country'];
+        $postal_code = $_POST['postal_code'];
+        $tel = $_POST['tel'];
+
+        $type = $_POST['type'];
+        $number = $_POST['number'];
+        $name = $_POST['name'];
+        $expiracy_date = $_POST['expiracy_date'];
+        $security_code = $_POST['security_code'];
+
+        require '../../../config.php';
+        $db_handle = mysqli_connect(DB_SERVER, DB_USER, DB_PASS );
+        $database = "amazonece";
+        $db_found = mysqli_select_db( $db_handle, $database );
+
+        if($db_found)
+        {
+            $SQL ="SELECT * from payment_info WHERE number=\"" . $number. "\"";
+            $result = mysqli_query($db_handle, $SQL);
+
+            while ($db_field = mysqli_fetch_assoc($result))
+            {
+
+                if($db_field['type'] == $type and $db_field['name'] == $name and $db_field['expiracy_date'] == $expiracy_date and $db_field['security_code'] == $security_code)
+                {
+                    echo "Informations bancaires OK<br/>";
+                    //A rajouter : email de l'acheteur à recup dans la session
+                    //           : Liste des id des items achetés à recupérer dans le panier (ou la session)
+                    //           : Prix de la commande à recuperer dans le panier (ou la session)
+                    $SQL ="INSERT INTO  purchase(card_number,adress,city,postal_code,contact_number,country,surname,firstname) VALUES(\"" . $number ."\",\"" . $adress ."\",\"" . $city ."\",\"" . $postal_code ."\",\"" . $tel ."\",\"" . $country ."\",\"" . $surname ."\",\"" . $firstname ."\")";
+                    if(mysqli_query($db_handle, $SQL))
+                    {
+                        echo "Commande enregistrée<br/>";
+                    }
+                    else echo "echec de la requete : ".$SQL ."<br/>Erreur :" . mysqli_error($db_handle);
+                }
+                else
+                {
+                    echo "Informations bancaires erronées<br/>" ;
+                }
+            }
+        }
+    }
+
---
+++ php/Payment_Form.php
@@ -0,0 +1,24 @@
+<div class="payment">
+    <form method="post" action="Pay_Order_Process.php">
+        Informations personnelles de livraison<br/>
+        <label for="surname">Nom </label><input type="text" name="surname" id="surname"><br/>
+        <label for="firstname">Prenom </label><input type="text" name="firstname" id="firstname"><br/>
+        <label for="adress">Adresse </label><input type="text" name="adress" id="adress"><br/>
+        <label for="city">Ville </label><input type="text" name="city" id="city"><br/>
+        <label for="postal_code">Code postal </label><input type="text" name="postal_code" id="postal_code"><br/>
+        <?php include "Country_list.php";?>
+        <label for="tel">Numero de telephone de contact</label><input type="text" name="tel" id="tel"><br/>
+        Informations relatives au paiement<br/>
+        <select name="type">
+            <option value="Visa">Visa</option>
+            <option value="MasterCard">MasterCard</option>
+            <option value="AmericanExpress">AmericanExpress</option>
+        </select>
+        <label for="number">Numero de la carte </label><input type="text" name="number" id="number"><br/>
+        <label for="name">Nom sur la carte </label><input type="text" name="name" id="name"><br/>
+        <label for="expiracy_date">Date d'expiration </label><input type="date" name="expiracy_date" id="expiracy_date"><br/>
+        <label for="security_code">Code de sécurité</label><input type="text" name="security_code" id="security_code"><br/><br/>
+        <input type="submit" value="Passer la commande">
+    </form>
+</div>
+
